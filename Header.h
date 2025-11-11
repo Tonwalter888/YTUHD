6a1875d20b386918f929489d4beab15e1ec752a3
Tonwalter888 <tonwater2545@gmail.com>
2025-11-11 14:48:33 +0700

    Cleanup

--- Header.h
+++ Header.h
@@ -4,20 +4,15 @@
 #import <YouTubeHeader/MLABRPolicyNew.h>
 #import <YouTubeHeader/MLABRPolicyOld.h>
 #import <YouTubeHeader/MLHAMPlayerItem.h>
-#import <YouTubeHeader/MLHAMQueuePlayer.h>
 #import <YouTubeHeader/MLHLSMasterPlaylist.h>
 #import <YouTubeHeader/MLHLSStreamSelector.h>
 #import <YouTubeHeader/MLFormat.h>
 #import <YouTubeHeader/HAMDefaultABRPolicy.h>
 #import <YouTubeHeader/YTIHamplayerConfig.h>
 #import <YouTubeHeader/YTIHamplayerStreamFilter.h>
-#import <YouTubeHeader/YTLocalPlaybackController.h>
-#import <YouTubeHeader/YTSingleVideoController.h>
-#import <YouTubeHeader/YTPlayerTapToRetryResponderEvent.h>
 
 #define IOS_BUILD "19H394"
 #define MAX_FPS 60
-#define MAX_HEIGHT 2160 // 4k
 #define MAX_PIXELS 8294400 // 3840 x 2160 (4k)
 
 #define UseVP9Key @"EnableVP9"
