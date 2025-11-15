28f43c186a98315de5db8da4975b8ecced57c72a
Tonwalter888 <tonwater2545@gmail.com>
2025-11-15 16:38:13 +0700

    Update Header.h

--- Header.h
+++ Header.h
@@ -2,7 +2,6 @@
 #define YTUHD_H_
 
 #import <Foundation/Foundation.h>
-#import <YouTubeHeader/MLABRPolicy.h>
 #import <YouTubeHeader/MLABRPolicyNew.h>
 #import <YouTubeHeader/MLABRPolicyOld.h>
 #import <YouTubeHeader/MLHAMPlayerItem.h>
